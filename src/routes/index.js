93b684250e9ee7ec9df7fe0b04a8b87e4424cb38
IrfanHakimm <mirfanhakim08@gmail.com>
2024-10-30 00:59:57 +0700

    fix: user routes & add post routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import postRoutes from "./post.routes.js";
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(authRoutes);
+router.use(postRoutes);
 
 export default router;
