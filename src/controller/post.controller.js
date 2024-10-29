0b079665308763b62067e5895dc0f86fa699781d
IrfanHakimm <mirfanhakim08@gmail.com>
2024-10-30 00:58:46 +0700

    feat: add CRD post controller

---
+++ src/controller/post.controller.js
@@ -0,0 +1,85 @@
+import {
+  CreatePost,
+  GetPosts,
+  GetPostById,
+  DeletePost,
+} from "../service/post.service.js";
+
+const _CreatePost = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const post = await CreatePost(req.body, userId);
+    if (post.error) {
+      return res.status(400).json({ message: post.error });
+    }
+
+    res.status(201).json({
+      status: "success",
+      message: "Post created successfully.",
+      data: post.data,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating post", error: error.message });
+  }
+};
+
+const _GetPosts = async (req, res) => {
+  try {
+    const posts = await GetPosts(req.body);
+    if (posts.error) {
+      return res.status(400).json({ message: posts.error });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Posts retrieved successfully.",
+      data: posts.data,
+      pagination: posts.pagination,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching posts", error });
+  }
+};
+
+const _GetPostById = async (req, res) => {
+  try {
+    const post = await GetPostById(req.params.id);
+    if (post.error) {
+      return res.status(400).json({ message: post.error });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Post retrieved successfully.",
+      data: post.data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching post", error });
+  }
+};
+
+const _DeletePost = async (req, res) => {
+  try {
+    const post = await DeletePost(req.params.id);
+    if (post.error) {
+      return res.status(400).json({ message: post.error });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Post deleted successfully.",
+      data: post.data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post", error });
+  }
+};
+
+export default {
+  _CreatePost,
+  _GetPosts,
+  _GetPostById,
+  _DeletePost,
+};
