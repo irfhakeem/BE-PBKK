2dbd367fd56296cc89bbb281a8855ba1d8a4a8c3
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-01 19:44:38 +0700

    feat: init storage routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import postRoutes from "./post.routes.js";
+import storageRoutes from "./storage.routes.js";
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(authRoutes);
 router.use(postRoutes);
+router.use(storageRoutes);
 
 export default router;
