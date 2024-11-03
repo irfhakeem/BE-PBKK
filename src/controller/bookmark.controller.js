bcdc1bc20f5b9c807d0452c458482e2e68c7e9d7
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-03 21:35:31 +0700

    feat: add bookmark controller

---
+++ src/controller/bookmark.controller.js
@@ -0,0 +1,62 @@
+import {
+  addBookmark,
+  deleteBookmark,
+  getUserBookmarks,
+} from "../service/bookmark.service.js";
+import messages from "../helpers/messages/bookmark.message.js";
+
+const _addBookmark = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const data = req.body;
+    const bookmark = await addBookmark(userId, data);
+
+    if (bookmark.error) {
+      return res.status(400).json({ error: bookmark.error });
+    }
+
+    return res.status(201).json({
+      status: "success",
+      message: messages.SuccessAddUserBookmark,
+      data: bookmark.data,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const _deleteBookmark = async (req, res) => {
+  try {
+    const result = await deleteBookmark(req.body);
+
+    if (result.error) {
+      return res.status(400).json({ error: result.error });
+    }
+
+    return res
+      .status(200)
+      .json({ message: messages.SuccessDeleteUserBookmark });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const _getUserBookmarks = async (req, res) => {
+  try {
+    const bookmarks = await getUserBookmarks(req.userId);
+
+    if (bookmarks.error) {
+      return res.status(400).json({ error: bookmarks.error });
+    }
+
+    return res.status(200).json({
+      status: "success",
+      message: messages.SuccessGetUserBookmark,
+      data: bookmarks.data,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export default { _addBookmark, _deleteBookmark, _getUserBookmarks };
