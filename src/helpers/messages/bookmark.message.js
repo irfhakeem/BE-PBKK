edc4fb03943d65bc8944b5c02c614c2d7b74b447
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-03 21:34:50 +0700

    feat: add bookmark message

---
+++ src/helpers/messages/bookmark.message.js
@@ -0,0 +1,11 @@
+var messages = {
+  ErrUserBookmarkNotFound: "User bookmark not found",
+  ErrAddUserBookmark: "Error while add user bookmark",
+  ErrDeleteUserBookmark: "Error while deleting user bookmark",
+
+  SuccessGetUserBookmark: "User bookmark retrieved successfully",
+  SuccessAddUserBookmark: "User bookmark add successfully",
+  SuccessDeleteUserBookmark: "User bookmark deleted successfully",
+};
+
+export default messages;
