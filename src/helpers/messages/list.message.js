a93430a8a4915eb2d3a9c72e469806a351414bad
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-03 21:35:03 +0700

    feat: add list message

---
+++ src/helpers/messages/list.message.js
@@ -0,0 +1,17 @@
+var messages = {
+  ErrGetList: "error reatriving list",
+  ErrCreateList: "error while creating list",
+  ErrUpdateList: "error while updating list",
+  ErrListNotFound: "list not found",
+  ErrGetSpecificList: "error reatriving specific list",
+  ErrDeleteList: "error while deleting list",
+
+  //   Success
+  SuccessGetList: "list retrieved successfully",
+  SuccessCreateList: "list created successfully",
+  SuccessDeleteList: "list deleted successfully",
+  SuccessUpdateList: "list updated successfully",
+  SuccessGetSpecificList: "specific list retrieved successfully",
+};
+
+export default messages;
