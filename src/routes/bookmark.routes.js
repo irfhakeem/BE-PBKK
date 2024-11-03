5d35425e61a3e5f3d3f48731b8fce3f68e1773ee
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-03 21:32:56 +0700

    feat: add bookmark routes

---
+++ src/routes/bookmark.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authenticate from "../middleware/middleware.js";
+import bookmarkController from "../controller/bookmark.controller.js";
+
+const router = express.Router();
+
+router.get("/bookmark/", authenticate, bookmarkController._getUserBookmarks);
+router.post("/bookmark/add", authenticate, bookmarkController._addBookmark);
+router.delete(
+  "/bookmark/remove",
+  authenticate,
+  bookmarkController._deleteBookmark
+);
+
+export default router;
