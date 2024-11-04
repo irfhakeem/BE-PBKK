ffba448837f55c328a78b3e47d3ab65f41e9283b
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-04 20:58:06 +0700

    fix: delete original bookmark routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import postRoutes from "./post.routes.js";
 import storageRoutes from "./storage.routes.js";
-import bookmarkRoutes from "./bookmark.routes.js";
 import listRoutes from "./list.routes.js";
 import tagRoutes from "./tag.routes.js";
 
@@ -13,7 +12,6 @@ router.use(userRoutes);
 router.use(authRoutes);
 router.use(postRoutes);
 router.use(storageRoutes);
-router.use(bookmarkRoutes);
 router.use(listRoutes);
 router.use(tagRoutes);
 
