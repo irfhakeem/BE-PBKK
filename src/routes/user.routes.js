e18e085464cd46bfb738f202c6f9bbe4e2e47971
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-04 12:56:43 +0700

    fix: routes order

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,6 +8,7 @@ router.get("/user/me", authenticate, userController._Me);
 router.patch("/user/update-profile", authenticate, userController._UpdateUser);
 router.delete("/user/delete-profile", authenticate, userController._DeleteUser);
 router.get("/user/random-users", authenticate, userController._GetRandomUsers);
+router.get("/user/following", authenticate, userController._GetFollowing);
 router.get("/user/:username", authenticate, userController._GetUserByUsername);
 router.patch("/user/deactivate", authenticate, userController._DeactivateUser);
 router.post("/user/follow", authenticate, userController._FollowUser);
