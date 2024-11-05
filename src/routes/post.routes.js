de30b4085fdcae2c14f10cf31f60600e0596b7ce
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-05 22:31:18 +0700

    feat: add like API routes

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -8,5 +8,8 @@ router.post("/post/create", authenticate, postController._CreatePost);
 router.post("/post", authenticate, postController._GetPosts);
 router.get("/post/:id", authenticate, postController._GetPostById);
 router.delete("/post/delete/:id", authenticate, postController._DeletePost);
+router.post("/post/like", authenticate, postController._LikePost);
+router.delete("/post/unlike", authenticate, postController._UnlikePost);
+router.post("/post/is-liked", authenticate, postController._IsPostLiked);
 
 export default router;
