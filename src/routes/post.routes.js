f958067573eb1ff79d838d3bc26e6789bb5bb19d
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-11 03:44:12 +0700

    feat: add get user posts route

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.post("/post/create", authenticate, postController._CreatePost);
 router.post("/post", authenticate, postController._GetPosts);
-router.get("/post/:id", authenticate, postController._GetPostById);
 router.delete("/post/delete/:id", authenticate, postController._DeletePost);
 router.post("/post/like", authenticate, postController._LikePost);
 router.delete("/post/unlike", authenticate, postController._UnlikePost);
@@ -14,5 +13,7 @@ router.post("/post/is-liked", authenticate, postController._IsPostLiked);
 router.post("/post/comment", authenticate, postController._CommentPost);
 router.delete("/post/uncomment", authenticate, postController._DeleteComment);
 router.post("/post/comments", authenticate, postController._GetComments);
+router.get("/post/user-posts", authenticate, postController._GetUserPosts);
+router.get("/post/:id", authenticate, postController._GetPostById);
 
 export default router;
